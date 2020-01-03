e99c78d11bd924e0e410f0c62d14ea287fc64db5
Mae Kangsadan <maemeier@live.com>
2020-01-03 23:11:32 +0100

    setup

---
+++ src/pages/homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../style.css";
+
+const Homepage = () => {
+  return (
+    <section>
+      <h1>Tea station </h1>
+    </section>
+  );
+};
+export default Homepage;
