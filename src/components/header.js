500a2dd3f4924468943bfc97270249eda8d7c0ba
Mae Kangsadan <maemeier@live.com>
2020-01-04 00:01:53 +0100

    style banner and header

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      Header components
+      <div className="banner">
+        <h3 className="banner-subtitle">Over one hunderd flavour of tea</h3>
+        <h1 className="banner-title"> Specially crafted tea</h1>
+        <button className="banner-btn">Expolre</button>
+      </div>
+    </div>
+  );
+};
+export default Header;
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "../style.css";
+import Navbar from "../components/navbar";
+import Header from "../components/header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 
 const Homepage = () => {
   return (
     <section>
+      <Navbar />
+      <Header />
       <h1>Tea station </h1>
+      {/*  <FontAwesomeIcon icon={faThumbsUp} />*/}
     </section>
   );
 };
