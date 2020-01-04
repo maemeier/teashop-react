f8b752ec3d4c8f37aaeca2a854bfcd7228f18b6b
Mae Kangsadan <maemeier@live.com>
2020-01-05 00:00:59 +0100

    nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <span className="nav-btn" id="nav-btn">
+        <FontAwesomeIcon icon={faBars} />
+      </span>
+
+      <nav className="navbar" id="navbar">
+        <div className="navbar-header">
+          <span className="nav-close" id="nav-close">
+            {" "}
+            <FontAwesomeIcon icon={faTimes} />
+          </span>
+        </div>
+        <ul className="nav-items">
+          <li>
+            <a href="#" className="nav-link">
+              {" "}
+              Home
+            </a>
+          </li>
+
+          <li>
+            <a href="#" className="nav-link">
+              {" "}
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#" className="nav-link">
+              {" "}
+              Products
+            </a>
+          </li>
+
+          <li>
+            <a href="#" className="nav-link">
+              {" "}
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
