2166175fc6509ff6080de44232d0c537e1b33212
Mae Kangsadan <maemeier@live.com>
2020-01-04 16:59:10 +0100

    products style

--- src/components/products.js
+++ src/components/products.js
@@ -1,46 +1,46 @@
 import React from "react";
 import About from "../img/tea1.jpg";
+import About1 from "../img/tea4.png";
 
 const Products = () => {
   return (
     <div className="products">
       <div className="section-center clearfix">
         <div className="products-info">
-          <h3 className="section-subtitle">Check out</h3>
-          <h2 className="section-title">our products</h2>
-        </div>
-        <p>
-          {" "}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti,
-          dolores.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Corrupti, dolores.Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Corrupti, dolores.
-        </p>
-        <a href="products.html" className="main-btn">
-          inventory
-        </a>
-        <div className="products-inventory" clearfix>
           <div>
-            <img src={About} alt="tea" className="products-img" />
-            <h3 className="products-title">Products Title</h3>
+            <h3 class="section-subtitle">check out</h3>
+            <h2 class="section-title text-white">our products</h2>
+          </div>
+          <p className="products-text">
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti,
+            dolores.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Corrupti, dolores.Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Corrupti, dolores.
+          </p>
+          <a href="products.html" className="main-btn">
+            inventory
+          </a>
+        </div>
+
+        <div className="products-inventory clearfix">
+          <div class="product">
+            <img src={About1} alt="tea" className="products-img" />
+            <h3 className="products-title ">Products Title</h3>
             <h3 className="products-price">6,99 CHF</h3>
           </div>
         </div>
 
-        <div className="products-inventory" clearfix>
-          <div>
-            <img src={About} alt="tea" className="products-img" />
-            <h3 className="products-title">Products Title</h3>
-            <h3 className="products-price">6,99 CHF</h3>
-          </div>
+        <div class="product">
+          <img src={About1} alt="tea" className="products-img" />
+          <h3 className="products-title">Products Title</h3>
+          <h3 className="products-price">6,99 CHF</h3>
         </div>
 
-        <div className="products-inventory" clearfix>
-          <div>
-            <img src={About} alt="tea" className="products-img" />
-            <h3 className="products-title">Products Title</h3>
-            <h3 className="products-price">6,99 CHF</h3>
-          </div>
+        <div class="product">
+          <img src={About1} alt="tea" className="products-img" />
+          <h3 className="products-title">Products Title</h3>
+          <h3 className="products-price">6,99 CHF</h3>
         </div>
       </div>
     </div>
