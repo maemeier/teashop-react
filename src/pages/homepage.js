2bcf21b22ad24c74fa2aa3b6bda1e1b7090c9ec6
Mae Kangsadan <maemeier@live.com>
2020-01-04 02:30:20 +0100

    hover

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,16 +2,14 @@ import React from "react";
 import "../style.css";
 import Navbar from "../components/navbar";
 import Header from "../components/header";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import Skill from "../components/skills";
 
 const Homepage = () => {
   return (
     <section>
       <Navbar />
       <Header />
-      <h1>Tea station </h1>
-      {/*  <FontAwesomeIcon icon={faThumbsUp} />*/}
+      <Skill />
     </section>
   );
 };
